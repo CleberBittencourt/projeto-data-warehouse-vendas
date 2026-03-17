96be02cd33f9a659a3069c8dc61ca2b845a7e45d
CleberBittencourt <bittencourtcleber@gmail.com>
2026-03-17 00:41:45 -0300

    FatoVendas.sql

--- SQL/Tabelas/FatoVendas.sql
+++ SQL/Tabelas/FatoVendas.sql
@@ -1,48 +1,16 @@
-USE [ProjetoVendas]
-GO
+CREATE TABLE FatoVendas (
+    VendasID INT IDENTITY(1,1) PRIMARY KEY,
+    ProdutoKey INT NOT NULL,
+    ClienteKey INT NOT NULL,
+    PedidoKey INT NOT NULL,
+    LojaKey INT NOT NULL,
+    DataKey INT NOT NULL,
+    Quantidade INT NOT NULL,
+    ValorTotal DECIMAL(10,2) NOT NULL,
 
-/****** Object:  Table [dbo].[FatoVendas]    Script Date: 3/16/2026 9:07:21 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[FatoVendas](
-	[VendasID] [int] IDENTITY(1,1) NOT NULL,
-	[ProdutoKey] [int] NOT NULL,
-	[ClienteKey] [int] NOT NULL,
-	[PedidoKey] [int] NOT NULL,
-	[LojaKey] [int] NOT NULL,
-	[DataKey] [int] NOT NULL,
-	[Quantidade] [int] NOT NULL,
-	[ValorTotal] [decimal](10, 2) NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[VendasID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[FatoVendas]  WITH CHECK ADD  CONSTRAINT [FK_FATOSVENDAS_DATA] FOREIGN KEY([DataKey])
-REFERENCES [dbo].[DimData] ([DataKey])
-GO
-
-ALTER TABLE [dbo].[FatoVendas] CHECK CONSTRAINT [FK_FATOSVENDAS_DATA]
-GO
-
-ALTER TABLE [dbo].[FatoVendas]  WITH CHECK ADD  CONSTRAINT [FK_FatoVendas_Loja] FOREIGN KEY([LojaKey])
-REFERENCES [dbo].[DimLoja] ([LojaKey])
-GO
-
-ALTER TABLE [dbo].[FatoVendas] CHECK CONSTRAINT [FK_FatoVendas_Loja]
-GO
-
-ALTER TABLE [dbo].[FatoVendas]  WITH CHECK ADD  CONSTRAINT [FK_FatoVendas_Produto] FOREIGN KEY([ProdutoKey])
-REFERENCES [dbo].[DimProduto] ([ProdutoKey])
-GO
-
-ALTER TABLE [dbo].[FatoVendas] CHECK CONSTRAINT [FK_FatoVendas_Produto]
-GO
-
-
+    -- Chaves estrangeiras
+    CONSTRAINT FK_FatoVendas_Produto FOREIGN KEY (ProdutoKey) REFERENCES DimProduto(ProdutoKey),
+    CONSTRAINT FK_FatoVendas_Cliente FOREIGN KEY (ClienteKey) REFERENCES DimCliente(ClienteKey),
+    CONSTRAINT FK_FatoVendas_Loja FOREIGN KEY (LojaKey) REFERENCES DimLoja(LojaKey),
+    CONSTRAINT FK_FatoVendas_Data FOREIGN KEY (DataKey) REFERENCES DimData(DataKey)
+);
