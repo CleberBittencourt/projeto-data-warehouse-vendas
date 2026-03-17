d48e64211246c3deb4a2131c1bce478b17a6eba1
CleberBittencourt <bittencourtcleber@gmail.com>
2026-03-17 00:37:51 -0300

    DimData.sql

--- SQL/Tabelas/DimData.sql
+++ SQL/Tabelas/DimData.sql
@@ -1,31 +1,15 @@
-USE [ProjetoVendas]
-GO
-
-/****** Object:  Table [dbo].[DimData]    Script Date: 3/16/2026 9:05:53 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[DimData](
-	[DataKey] [int] NOT NULL,
-	[DataCompleta] [date] NOT NULL,
-	[Ano] [int] NOT NULL,
-	[MesNumero] [int] NOT NULL,
-	[MesNome] [nvarchar](20) NOT NULL,
-	[Trimestre] [int] NOT NULL,
-	[Dia] [int] NOT NULL,
-	[DiaSemanaNumero] [int] NOT NULL,
-	[DiaSemanaNome] [nvarchar](20) NOT NULL,
-	[SemanaAno] [int] NOT NULL,
-	[EhFimDeSemana] [bit] NOT NULL,
-	[EhDiaUtil] [bit] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[DataKey] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
+CREATE TABLE DimData (
+    DataKey INT PRIMARY KEY,
+    DataCompleta DATE NOT NULL,
+    Ano INT NOT NULL,
+    MesNumero INT NOT NULL,
+    MesNome NVARCHAR(20) NOT NULL,
+    Trimestre INT NOT NULL,
+    Dia INT NOT NULL,
+    DiaSemanaNumero INT NOT NULL,
+    DiaSemanaNome NVARCHAR(20) NOT NULL,
+    SemanaAno INT NOT NULL,
+    EhFimDeSemana BIT NOT NULL,
+    EhDiaUtil BIT NOT NULL
+);
 
