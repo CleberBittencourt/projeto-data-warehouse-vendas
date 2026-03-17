15b21c8970b2b7cfc1f5e0b8ce215c5f8906e66a
CleberBittencourt <bittencourtcleber@gmail.com>
2026-03-17 00:39:06 -0300

    DimCliente.sql

--- SQL/Tabelas/DimCliente.sql
+++ SQL/Tabelas/DimCliente.sql
@@ -1,22 +1,15 @@
-CREATE TABLE [dbo].[DimCliente](
-	[ClienteKey] [int] IDENTITY(1,1) NOT NULL,
-	[ClienteCodigoERP] [int] NULL,
-	[Nome] [varchar](100) NULL,
-	[Email] [varchar](100) NULL,
-	[Sexo] [varchar](10) NULL,
-	[DataNascimento] [date] NULL,
-	[Cidade] [varchar](100) NULL,
-	[Estado] [varchar](50) NULL,
-	[Pais] [varchar](50) NULL,
-	[DataCadastro] [date] NULL,
-	[DataInicio] [date] NULL,
-	[DataFim] [date] NULL,
-	[IsAtual] [bit] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[ClienteKey] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-
+CREATE TABLE DimCliente (
+    ClienteKey INT IDENTITY(1,1) PRIMARY KEY,
+    ClienteCodigoERP INT,
+    Nome VARCHAR(100),
+    Email VARCHAR(100),
+    Sexo VARCHAR(10),
+    DataNascimento DATE,
+    Cidade VARCHAR(100),
+    Estado VARCHAR(50),
+    Pais VARCHAR(50),
+    DataCadastro DATE,
+    DataInicio DATE,
+    DataFim DATE,
+    IsAtual BIT
+);
