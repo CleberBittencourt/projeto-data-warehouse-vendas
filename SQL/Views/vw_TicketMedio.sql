d9add1f4c8ff8fa641f0638d7391471568738143
CleberBittencourt <bittencourtcleber@gmail.com>
2026-03-17 00:32:37 -0300

    vw_TicketMedio.sql

--- SQL/Views/vw_TicketMedio.sql
+++ SQL/Views/vw_TicketMedio.sql
@@ -6,8 +6,3 @@ SELECT
     SUM(ValorTotal) / COUNT(DISTINCT PedidoKey) AS TicketMedio
 FROM FatoVendas
 GO
-/****** Object:  View [dbo].[vw_TopClientes]    Script Date: 3/16/2026 9:14:47 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
